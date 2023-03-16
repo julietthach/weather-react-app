d75ad35a55210048e64f468c3bb501574408677d
julietthach <119014732+julietthach@users.noreply.github.com>
2023-03-16 13:23:49 -0400

    Add Axios and Weather API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import WeatherApp from "./WeatherApp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WeatherApp />
+      <small class="author"><a href="https://github.com/julietthach/weather-react-app" target="_blank" rel="noreferrer" >Open-source code</a>, by Thi Thach</small>
     </div>
   );
 }
